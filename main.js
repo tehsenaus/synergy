ada02792fae87793ffa2d3e02119ac265606b332
Sean Micklethwaite <sean@dysoft.co.uk>
2011-07-31 11:16:11 +0100

    Package

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
 var fs = require('fs');
 var connect = require("connect");
-var browserify = require("browserify");
+var browserify = require("konode_browserify");
 var _ = require("underscore");
+var jsdom = require("jsdom");
 
 var defaults = {
 	host: "localhost",
@@ -21,26 +22,52 @@ module.exports = function(options) {
 		"utf8"
 	);
 	
+	var konode_js = browserify({
+		mount: "/site.js",
+		require: ["konode/client", "./viewmodel"]
+	});
+	
+	// Add libraries to bundle
+	(function buildLibs(dir) {
+		fs.readdirSync(dir).sort().forEach(function(f) {
+			if(f[0] == '.') return;
+			
+			f = dir + '/' + f;
+			if(fs.statSync(f).isDirectory()) {
+				buildLibs(f);
+			} else {
+				konode_js.append(fs.readFileSync(f));
+			}
+		});
+	})(__dirname + "/lib");
+	
 	
 	var server = connect(
 		connect.vhost(options.libHost || options.host, connect(
-			browserify({
-				require: __dirname + "/client/index.js",
-				mount: "/konode.js"
-			}),
-			browserify({
-				require: [{us: "underscore"}, "./routes"],
-				mount: "/site.js",
-				root: __dirname
-			})
+			konode_js
 		)),
+		
+		// Template server (main index)
 		connect.vhost(options.host, connect(
-			function(req, res) {
-				res.end(template);
+			function(req, response) {
+				if(req.url == "/" || req.url == "")
+					response.end(template);
+				else {
+					// Redirect to homepage with path in hash
+					response.statusCode = 302;
+					response.setHeader("Location", "/#" + req.url);
+					response.end();
+				}
 			}
 		))
 		
 	).listen(options.port);
 	
+	//var wnd = jsdom.jsdom(template).createWindow();
+	//console.log(wnd);
+	
 	return server;
 };
+
+module.exports.core = require("konode/core");
+//module.exports.server = require("konode/server");
